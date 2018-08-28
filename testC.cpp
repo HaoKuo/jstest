e8f90f9565b79cc552abed102ffb869972966660
HaoKuo <mailhao@163.com>
2018-08-28 09:59:29 +0800

    test cin

--- testC.cpp
+++ testC.cpp
@@ -3,6 +3,20 @@ using namespace std;
 
 int main()
 {
-    cout<<"Hello World!"<<endl;
+    int currVal = 0, val = 0;
+    if (cin >> currVal){
+        int cnt = 0;
+        while (cin >> val) {
+            if (val == currVal)
+                ++cnt;
+            else {
+                cout << currVal << " occurs "
+                     << cnt << " times" << endl;
+                     currVal = val;
+                     cnt = 1;
+            }
+        }
+
+    }
     return 0;
 }
