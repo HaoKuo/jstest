e5bdebe6a48c9048cf15a7ba12f47129be0aa715
HaoKuo <mailhao@163.com>
2018-08-28 10:23:17 +0800

    test1

--- testC.cpp
+++ testC.cpp
@@ -5,18 +5,19 @@ int main()
 {
     int currVal = 0, val = 0;
     if (cin >> currVal){
-        int cnt = 0;
+        int cnt = 1;
         while (cin >> val) {
             if (val == currVal)
                 ++cnt;
             else {
                 cout << currVal << " occurs "
                      << cnt << " times" << endl;
-                     currVal = val;
-                     cnt = 1;
+                currVal = val;
+                cnt = 1;
             }
         }
-
+        cout << currVal << " occurs "
+             << cnt << " times" << endl;
     }
     return 0;
 }
